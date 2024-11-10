a8a2bc72a87c27b56f1ce9d76222c5abe119cda2
sriarya <chaithracsy@gmail.com>
2024-11-10 15:43:33 +0530

    leraning variable initiation and assignment

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//go is a static language like  C++ and Java
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades" // here the type is being inferred by go
+	//and also use : for the very first initialisation
+	//No need to use : for reassignment
+	card = "Five of Diamonds"
+	fmt.Println(card)
 }
