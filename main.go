c0ed808691eeebb7580e8cbcbca6e8b613c5e360
sriarya <chaithracsy@gmail.com>
2024-11-10 15:47:07 +0530

    checkig out functions that returns some values

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import "fmt"
 func main() {
 	//go is a static language like  C++ and Java
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades" // here the type is being inferred by go
+	//card := "Ace of Spades" // here the type is being inferred by go
 	//and also use : for the very first initialisation
 	//No need to use : for reassignment
-	card = "Five of Diamonds"
+	card := newCard()
 	fmt.Println(card)
+}
+
+// When retutning something from a function do mention the type of variable being returned
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
+
+func newCard() int {
+	return 32
 }
