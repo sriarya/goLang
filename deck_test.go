33900a5ee8737825464ed7b5815668a45b9d1f0a
sriarya <chaithracsy@gmail.com>
2024-11-15 20:52:20 +0530

    adding one more test case

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDock(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,19 @@ func TestNewDock(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last card  Four of Clubs but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck of length 16 but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
